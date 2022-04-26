839e435692b977bcff8fedef7a16e55b8a396d71
zhika <zsultanova03@gmail.com>
2022-04-26 16:50:49 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import {data} from "./data"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const [change, setChange] = useState();
 
-export default App;
+ const toggle= i=>{
+  if(change===i){
+    return setChange()
+  }
+  setChange(i)
+ }
+  
+  return (
+ <div className="wrapper">
+   <h1>Questions And <br/>
+   Answers About<br/>
+    Login</h1>
+   <div className="container">
+     {data.map((item, i)=>(
+       <div className="item">
+         <div className="title" onClick={()=>{
+           toggle(i)
+         }}>
+           <h2>{item.title}</h2>
+           <span>{change===i?"-":"+"}</span>
+         </div>
+        
+         <div className={change ===i ? "content show" : "content"}>
+           <p>{item.info}</p>
+         </div>
+       </div>
+     ))}
+   </div>
+ </div>
+  )
+}
+export default App
